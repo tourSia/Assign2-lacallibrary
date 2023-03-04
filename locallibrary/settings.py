83804be6bd2402bf2969b69907fbae53f1640053
tourSia <124312754+tourSia@users.noreply.github.com>
2023-03-04 15:29:33 -0600

    update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%+@7ktjw!d*!qwf#p6dfp4+epbcjx_2mw=^nj!f76f8u$kitc$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [] #['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
